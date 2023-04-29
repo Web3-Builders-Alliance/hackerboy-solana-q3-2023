52ef49306f67edf9569736d9ec4c72eb87c88b34
raghav-rama <hackerer528@gmail.com>
2023-04-29 13:50:35 +0530

    feat(rust): add hello world program

---
+++ rust/hello/target/debug/deps/hello-4ae467a9e4c93fc8.d
@@ -0,0 +1,5 @@
+/home/hackerboy/solana-programs/WBA-PreReqs/rust/hello/target/debug/deps/hello-4ae467a9e4c93fc8: src/main.rs
+
+/home/hackerboy/solana-programs/WBA-PreReqs/rust/hello/target/debug/deps/hello-4ae467a9e4c93fc8.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/hackerboy/solana-programs/WBA-PreReqs/rust/hello/target/debug/hello: /home/hackerboy/solana-programs/WBA-PreReqs/rust/hello/src/main.rs
