1812172fa5b2ed605916eeae7941e4ea3c9bcc80
raghav-rama <hackerer528@gmail.com>
2023-04-30 19:51:09 +0530

    feat(rust): add lesson types

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,9 +1,12 @@
 mod print;
 mod vars;
+mod types;
 
 fn main() {
     println!("mod print::run()");
     print::run();
     println!("\n\nmod vars::run()");
     vars::run();
+    println!("\n\nmod types::run()");
+    types::run();
 }
--- rust/hello/target/release/deps/hello-65c9d9d1af539411.d
+++ rust/hello/target/release/deps/hello-65c9d9d1af539411.d
@@ -1,7 +1,8 @@
-/home/hackerboy/solana-programs/WBA-PreReqs/rust/hello/target/release/deps/hello-65c9d9d1af539411: src/main.rs src/print.rs src/vars.rs
+/home/hackerboy/solana-programs/WBA-PreReqs/rust/hello/target/release/deps/hello-65c9d9d1af539411: src/main.rs src/print.rs src/vars.rs src/types.rs
 
-/home/hackerboy/solana-programs/WBA-PreReqs/rust/hello/target/release/deps/hello-65c9d9d1af539411.d: src/main.rs src/print.rs src/vars.rs
+/home/hackerboy/solana-programs/WBA-PreReqs/rust/hello/target/release/deps/hello-65c9d9d1af539411.d: src/main.rs src/print.rs src/vars.rs src/types.rs
 
 src/main.rs:
 src/print.rs:
 src/vars.rs:
+src/types.rs:
