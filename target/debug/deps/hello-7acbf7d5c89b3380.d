520a8f3c8f74439ab28f69fbc1d2bcb0ccbd5474
raghav-rama <hackerer528@gmail.com>
2023-08-15 14:58:17 +0530

    Delete `target` folder

--- target/debug/deps/hello-7acbf7d5c89b3380.d
+++
@@ -1,8 +0,0 @@
-/home/hackerboy/solana-programs/WBA-PreReqs/target/debug/deps/hello-7acbf7d5c89b3380.rmeta: rust/hello/src/main.rs rust/hello/src/print.rs rust/hello/src/vars.rs rust/hello/src/types.rs
-
-/home/hackerboy/solana-programs/WBA-PreReqs/target/debug/deps/hello-7acbf7d5c89b3380.d: rust/hello/src/main.rs rust/hello/src/print.rs rust/hello/src/vars.rs rust/hello/src/types.rs
-
-rust/hello/src/main.rs:
-rust/hello/src/print.rs:
-rust/hello/src/vars.rs:
-rust/hello/src/types.rs:
