e08519163baa14cbf2815c5e9abee3fcc0f70f24
raghav-rama <hackerer528@gmail.com>
2023-08-22 20:18:46 +0530

    feat: add `vault_withdrawal.ts`

---
+++ ts/src/cluster1/vault_withdrawal.ts
@@ -0,0 +1,60 @@
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Commitment,
+  LAMPORTS_PER_SOL,
+} from '@solana/web3.js';
+import {
+  Program,
+  AnchorProvider,
+  Wallet,
+  Address,
+  BN,
+} from '@project-serum/anchor';
+import { WbaVault, IDL } from '../programs/wba_vault_program';
+import wallet from '../../wba-wallet.json';
+
+const payer = Keypair.fromSecretKey(new Uint8Array(wallet));
+const commitment: Commitment = 'confirmed';
+const connection = new Connection('https://api.devnet.solana.com', commitment);
+const provider = new AnchorProvider(connection, new Wallet(payer), {
+  commitment,
+});
+const program = new Program<WbaVault>(
+  IDL,
+  'D51uEDHLbWAxNfodfQDv7qkp8WZtxrhi3uganGbNos7o' as Address,
+  provider
+);
+const vaultState = new PublicKey(
+  '5oSurGpnKu92rTmNEwUMyKbKTyXeyWHYBbqBV1EW9j71'
+);
+const [vaultAuth, _vaultAuth_bump] = PublicKey.findProgramAddressSync(
+  [Buffer.from('auth'), Buffer.from(vaultState.toBuffer())],
+  program.programId
+);
+const [vault, _vault_bump] = PublicKey.findProgramAddressSync(
+  [Buffer.from('vault'), Buffer.from(vaultAuth.toBuffer())],
+  program.programId
+);
+
+
+(async () => {
+  try {
+    const txId = await program.methods
+      .withdraw(new BN(0.1 * LAMPORTS_PER_SOL))
+      .accounts({
+        owner: payer.publicKey,
+        vault,
+        vaultAuth,
+        vaultState,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([payer])
+      .rpc();
+    console.log(`txId: ${txId}`);
+  } catch (error) {
+    console.log(`some error occurred: ${error}`);
+  }
+})();
